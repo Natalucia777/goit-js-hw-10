fce491db34c2ac68910d6d803b4ed68132f537e4
Natalya Demidenko <natalucia777@ukr.net>
2023-05-03 13:09:51 +0300

    hw-10-1-3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from "notiflix";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector("input.search-box");
+const countryList = document.querySelector("ul.country-list");
+const countryInfo = document.querySelector("div.country-info");
 
+const cleanMarkup = ref => (ref.innerHTML = '');
+const onInput = e => {
+  const inputText = e.target.value.trim();
+  if (!inputText) {
+    cleanMarkup(countryList);
+    cleanMarkup(countryInfo);
+    return;
+  }
+}
+fetchCountries(inputText).then(data => {
+  console.log(data);
+  if (data.lenght > 10) {
+    
+  }
+})
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,8 @@
+// const DATA_URL = 'https://restcountries.com/v3.1/all';
+const DATA_URL = 'https://restcountries.com/v3.1/name';
+const fetchCountries = {
+  
+}
+
+
+export { fetchCountries };
