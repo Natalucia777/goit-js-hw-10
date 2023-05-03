ec5c5adf222b6c78f2d9f78808e1f9f32ed9153f
Natalya Demidenko <natalucia777@ukr.net>
2023-05-04 01:17:27 +0300

    hw-10-1-19

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCountries } from './js/fetchCountries';
+import { fetchCountries } from './js/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector('input.search-box');
